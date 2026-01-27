05e82b6e978cdfcdbfd903c2f8d95428dea69b12
Katam Veera Kowshik <veerakowshikkatam@gmail.com>
2026-01-27 23:16:36 +0530

    Added ChatPromptTemplate

---
+++ From Youtube/Prompts/Chatprompttemplate.py
@@ -0,0 +1,25 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_ollama import ChatOllama
+
+# chat_template = ChatPromptTemplate([
+# SystemMessage(content="You are a helpful {domain} expert."),
+# HumanMessage(content= "Explain about {topic}")
+# ])
+# prompt = chat_template.invoke({"domain": "cricket", "topic": "doosra"})
+
+# print(prompt) 
+"""messages=[SystemMessage(content='You are a helpful {domain} expert.', additional_kwargs={}, response_metadata={}), HumanMessage(content='Explain about {topic}', additional_kwargs={}, response_metadata={})]"""
+
+chat_template = ChatPromptTemplate([
+    ('system', 'You are a helpful {domain} expert.'),
+    ('human', 'Explain about {topic}')
+])
+prompt = chat_template.invoke({"domain": "cricket", "topic": "doosra"})
+
+print(prompt)
+"""messages=[SystemMessage(content='You are a helpful cricket expert.', additional_kwargs={}, response_metadata={}), HumanMessage(content='Explain about doosra', additional_kwargs={}, response_metadata={})]"""
+
+llm = ChatOllama(model = "llama3.2")
+response = llm.invoke(prompt)
+print(response.content)
