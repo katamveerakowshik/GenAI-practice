9305f3e4f6004179a3a72d2161724e2c0e9bdb9d
Katam Veera Kowshik <veerakowshikkatam@gmail.com>
2026-03-18 01:08:37 +0530

    added resume in chatbot, buttons needed to be added in sidebar

---
+++ Langgraph/Chatbot with UI/3_streamlit_with_threading.py
@@ -0,0 +1,67 @@
+
+#********************************Importing necessary libraries and modules********************************
+import streamlit as st
+from langgraph_backend import workflow, ChatState
+from langchain_core.messages import HumanMessage
+import uuid
+
+
+
+#***********************************Utility functions***********************************
+def generate_thread_id():
+    return str(uuid.uuid4())
+
+def new_chat():
+    st.session_state.thread_id = generate_thread_id()
+    st.session_state.thread_list.append(st.session_state.thread_id)
+    st.session_state.message_history = []
+
+
+#***********************************Session states ************************************
+# creating a session state to store previous messages
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'thread_list' not in st.session_state:
+    st.session_state['thread_list'] = [st.session_state.thread_id]
+
+#********************************Displaying previous messages in the chat UI********************************
+for message in st.session_state.message_history:
+    role = message["role"]
+    text = message["text"]
+    with st.chat_message(role):
+        st.text(text)
+
+
+#*******************************UI for user input and displaying AI response********************************
+st.sidebar.title("Langgraph Chatbot")
+if st.sidebar.button("New Chat"):
+    new_chat()
+    st.rerun()
+
+st.sidebar.header("Chat History")
+for thread_id in st.session_state.thread_list[::-1]:
+    st.sidebar.button(str(thread_id))
+
+user_input = st.chat_input("Ask something")
+
+if user_input:
+    st.session_state["message_history"].append({"role": "user", "text": user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+
+
+    CONFG = {"configurable": {"thread_id": st.session_state.thread_id}}
+
+    input_state = ChatState(messages=[HumanMessage(user_input)])
+    
+
+    with st.chat_message("ai"):
+        ai_messaege = st.write_stream(
+        message_chunk.content for message_chunk, metadata in workflow.stream(input_state, config=CONFG, stream_mode="messages")
+        )
+        st.session_state.message_history.append({"role": "ai", "text": ai_messaege})
+    
